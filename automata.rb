954bc364e7fd8901c46815b3894d5dd2afa97a43
Dan Dorman <dan.dorman@gmail.com>
2013-08-22 09:59:49 -0600

    Continue work in the automata section

--- automata.rb
+++ automata.rb
@@ -62,9 +62,19 @@ NFARulebook = Struct.new(:rules) do
   def rules_for(state, character)
     rules.select { |rule| rule.applies_to?(state, character) }
   end
+
+  def follow_free_moves(states)
+    more_states = next_states(states, nil)
+
+    if more_states.subset?(states)
+      states
+    else
+      follow_free_moves(states + more_states)
+    end
+  end
 end
 
-NFA = Struct.new(:current_states, :accept_states, :rulebook) do
+class NFA < Struct.new(:current_states, :accept_states, :rulebook)
   def accepting?
     (current_states & accept_states).any?
   end
@@ -78,11 +88,15 @@ NFA = Struct.new(:current_states, :accept_states, :rulebook) do
       read_character(character)
     end
   end
+
+  def current_states
+    rulebook.follow_free_moves(super)
+  end
 end
 
 NFADesign = Struct.new(:start_state, :accept_states, :rulebook) do
   def to_nfa
-    NFA.new([start_state], accept_states, rulebook)
+    NFA.new(Set[start_state], accept_states, rulebook)
   end
 
   def accepts?(string)
@@ -110,11 +124,11 @@ end
 # p dfa_design.accepts?("baa")
 # p dfa_design.accepts?("baba")
 
-rulebook = NFARulebook.new([
-  FARule.new(1, ?a, 1), FARule.new(1, ?b, 1), FARule.new(1, ?b, 2),
-  FARule.new(2, ?a, 3), FARule.new(2, ?b, 3),
-  FARule.new(3, ?a, 4), FARule.new(3, ?b, 4)
-])
+# rulebook = NFARulebook.new([
+#   FARule.new(1, ?a, 1), FARule.new(1, ?b, 1), FARule.new(1, ?b, 2),
+#   FARule.new(2, ?a, 3), FARule.new(2, ?b, 3),
+#   FARule.new(3, ?a, 4), FARule.new(3, ?b, 4)
+# ])
 # p rulebook.next_states(Set[1], ?b)
 # p rulebook.next_states(Set[1, 2], ?a)
 # p rulebook.next_states(Set[1, 3], ?b)
@@ -129,7 +143,24 @@ rulebook = NFARulebook.new([
 # nfa = NFA.new(Set[1], [4], rulebook); p nfa.accepting?
 # nfa.read_string(?b * 5); p nfa.accepting?
 
-nfa_design = NFADesign.new(1, [4], rulebook)
-p nfa_design.accepts?("bab")
-p nfa_design.accepts?("bbbbb")
-p nfa_design.accepts?("bbabb")
+# nfa_design = NFADesign.new(1, [4], rulebook)
+# p nfa_design.accepts?("bab")
+# p nfa_design.accepts?("bbbbb")
+# p nfa_design.accepts?("bbabb")
+
+rulebook = NFARulebook.new([
+  FARule.new(1, nil, 2), FARule.new(1, nil, 4),
+  FARule.new(2, ?a, 3),
+  FARule.new(3, ?a, 2),
+  FARule.new(4, ?a, 5),
+  FARule.new(5, ?a, 6),
+  FARule.new(6, ?a, 4)
+])
+
+# p rulebook.next_states(Set[1], nil)
+
+nfa_design = NFADesign.new(1, [2, 4], rulebook)
+p nfa_design.accepts?(?a * 2)
+p nfa_design.accepts?(?a * 3)
+p nfa_design.accepts?(?a * 5)
+p nfa_design.accepts?(?a * 6)
