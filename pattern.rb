fded3bbfbd3a686ac5c100341281969d3f494155
Dan Dorman <dan.dorman@gmail.com>
2013-08-23 08:36:33 -0600

    Get rid of examples

--- pattern.rb
+++ pattern.rb
@@ -137,62 +137,6 @@ module Pattern
       NFADesign.new(start_state, accept_states, rulebook)
     end
   end
-
-  # p pattern = Repeat.new(
-  #   Choose.new(
-  #     Concatenate.new(Literal.new(?a), Literal.new(?b)),
-  #     Literal.new(?a)
-  #   )
-  # )
-
-  # nfa_design = Empty.new.to_nfa_design
-  # p nfa_design.accepts?("")
-  # p nfa_design.accepts?(?a)
-
-  # nfa_design = Literal.new(?a).to_nfa_design
-  # p nfa_design.accepts?("")
-  # p nfa_design.accepts?(?a)
-  # p nfa_design.accepts?(?b)
-
-  # p Empty.new.matches?(?a)
-  # p Literal.new(?a).matches?(?a)
-
-  # p pattern = Concatenate.new(
-  #   Literal.new(?a),
-  #   Concatenate.new(Literal.new(?b), Literal.new(?c))
-  # )
-
-  # p pattern.matches?("a")
-  # p pattern.matches?("ab")
-  # p pattern.matches?("abc")
-
-  # p pattern = Choose.new(Literal.new(?a), Literal.new(?b))
-
-  # p pattern.matches?(?a)
-  # p pattern.matches?(?b)
-  # p pattern.matches?(?c)
-
-  # p pattern = Repeat.new(Literal.new(?a))
-
-  # p pattern.matches?(?a * 0)
-  # p pattern.matches?(?a * 1)
-  # p pattern.matches?(?a * 4)
-  # p pattern.matches?(?b * 1)
-
-  # p pattern = Repeat.new(
-  #   Concatenate.new(
-  #     Literal.new(?a),
-  #     Choose.new(Empty.new, Literal.new(?b))
-  #   )
-  # )
-
-  # p pattern.matches?("")
-  # p pattern.matches?("a")
-  # p pattern.matches?("ab")
-  # p pattern.matches?("aba")
-  # p pattern.matches?("abab")
-  # p pattern.matches?("abaab")
-  # p pattern.matches?("abba")
 end
 
 require "treetop"
