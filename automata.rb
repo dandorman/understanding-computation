0ae704235ad0cbab140326a6ace585e519e50704
Dan Dorman <dan.dorman@gmail.com>
2013-08-24 17:35:49 -0600

    Finish up NFA->DFA section

--- automata.rb
+++ automata.rb
@@ -72,6 +72,10 @@ NFARulebook = Struct.new(:rules) do
       follow_free_moves(states + more_states)
     end
   end
+
+  def alphabet
+    rules.map(&:character).compact.uniq
+  end
 end
 
 class NFA < Struct.new(:current_states, :accept_states, :rulebook)
@@ -101,5 +105,38 @@ NFADesign = Struct.new(:start_state, :accept_states, :rulebook) do
 
   def accepts?(string)
     to_nfa.tap { |nfa| nfa.read_string(string) }.accepting?
+  end
+end
+
+NFASimulation = Struct.new(:nfa_design) do
+  def next_state(state, character)
+    nfa_design.to_nfa(state).tap { |nfa|
+      nfa.read_character(character)
+    }.current_states
+  end
+
+  def rules_for(state)
+    nfa_design.rulebook.alphabet.map { |character|
+      FARule.new(state, character, next_state(state, character))
+    }
+  end
+
+  def discover_states_and_rules(states)
+    rules = states.flat_map { |state| rules_for(state) }
+    more_states = rules.map(&:follow).to_set
+
+    if more_states.subset?(states)
+      [states, rules]
+    else
+      discover_states_and_rules(states + more_states)
+    end
+  end
+
+  def to_dfa_design
+    start_state   = nfa_design.to_nfa.current_states
+    states, rules = discover_states_and_rules(Set[start_state])
+    accept_states = states.select { |state| nfa_design.to_nfa(state).accepting? }
+
+    DFADesign.new(start_state, accept_states, DFARulebook.new(rules))
   end
 end
